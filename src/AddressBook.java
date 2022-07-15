c1eed5220df32714e1d28b41c08387ac5549d8f4
Your Name <you@example.com>
2022-07-15 19:20:52 +0530

    Added: ability to create a contact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,19 @@
-
+import java.util.ArrayList;
 public class AddressBook {
+	 private ArrayList<ContactPerson> contactBook ;
 
+	    public AddressBook(ArrayList<ContactPerson> contactBook) {
+	        this.contactBook = contactBook;
+	    }
+
+	    public ArrayList<ContactPerson> getContactBook() {
+	        return contactBook;
+	    }
+
+	    public void setContactBook(ArrayList<ContactPerson> contactBook) {
+	        this.contactBook = contactBook;
+	    }
+	
 }
+
+
